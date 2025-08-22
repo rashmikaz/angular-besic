c6762028f4bc9c607ec9f7d04b3a4a1be9928b4f
rashmikaz <phrashmika@gmail.com>
2025-08-23 03:50:30 +0530

    create component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  template: '<h1>Login Component</h1>',
+})
+export class LoginComponent {
+  // Component logic goes here
+}
